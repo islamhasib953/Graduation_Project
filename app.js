5306fc6bf6f158f24d1f4a19a3ac6f1e6714c6c6
islamhasib953 <islamhasib953@example.com>
2025-02-14 19:38:42 +0200

    vaccination

--- app.js
+++ app.js
@@ -27,47 +27,47 @@ const vaccinationRoutes = require("./routes/vaccination.route");
 dotenv.config({ path: './.env' });
 const app = express();
 
-app.use(session({
-  secret: "secret",  //process.env.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: true,
-  // cookie: { secure: process.env.NODE_ENV === 'production' }
-}))
+// app.use(session({
+//   secret: "secret",  //process.env.SESSION_SECRET,
+//   resave: false,
+//   saveUninitialized: true,
+//   // cookie: { secure: process.env.NODE_ENV === 'production' }
+// }))
 
-app.use(passport.initialize);
-app.use(passport.session());
+// app.use(passport.initialize);
+// app.use(passport.session());
 
-passport.serializeUser(function(user, done) {
-  done(null, user);
-});
+// passport.serializeUser(function(user, done) {
+//   done(null, user);
+// });
 
-passport.deserializeUser(function(user, done) {
-  done(null, user);
-});
+// passport.deserializeUser(function(user, done) {
+//   done(null, user);
+// });
 
-passport.use(new GoogleStrategy({
-  clientID: process.env.GOOGLE_CLIENT_ID,
-  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: process.env.GOOGLE_CALLBACK_URL,
-  scope: ['profile', 'email']
-  },
-  async (accessToken, refreshToken, profile, done) => {
-    try {
-      const user = await User.findOne({ googleId: profile.id });
-      if (user) {
-        return done(null, user);
-      } else {
-        const newUser = await User.create({
-          googleId: profile.id,
-          name: profile.displayName,
-          email: profile.emails[0].value
-        });
-        return done(null, newUser);
-      }
-    } catch (error) {
-      done(error);
-    }
-  }
+// passport.use(new GoogleStrategy({
+//   clientID: process.env.GOOGLE_CLIENT_ID,
+//   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+//   callbackURL: process.env.GOOGLE_CALLBACK_URL,
+//   scope: ['profile', 'email']
+//   },
+//   async (accessToken, refreshToken, profile, done) => {
+//     try {
+//       const user = await User.findOne({ googleId: profile.id });
+//       if (user) {
+//         return done(null, user);
+//       } else {
+//         const newUser = await User.create({
+//           googleId: profile.id,
+//           name: profile.displayName,
+//           email: profile.emails[0].value
+//         });
+//         return done(null, newUser);
+//       }
+//     } catch (error) {
+//       done(error);
+//     }
+//   }
 
 app.use("/uploads", express.static("uploads"));
 
